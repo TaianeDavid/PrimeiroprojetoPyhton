2098101ea007c0909fd02a2209755fdfd0f50240
Taiane David <taianedavid@gmail.com>
2022-04-08 19:16:56 -0300

    primeiro projeto

---
+++ projetocores.py
@@ -0,0 +1,43 @@
+nome=input('Qual o seu nome?')
+print('Olá', nome, '!','Prazer em te conhecer!') 
+pergunta=input('Vamos descobrir sua cor preferia? Sim ou não?')
+#if pergunta == sim:
+pergunta =int(input('Digite um número de 1 à 10    '))
+#if  pergunta ==não:   
+ #   print ('Volte outra hora!')
+    
+#pergunta =int(input('Digite um número de 1 à 10    '))
+#teste =  pergunta
+if pergunta == 1:
+    print ('\033[1;30;43m sua cor preferida', 'é' ,'Amarelo 😍\033[m')
+    print ('Fim!')
+if pergunta == 2:
+    print ('\033[1;30;42m sua cor preferida', 'é' , 'Verde' ' '  'a cor da esperança\033[m')
+    print ('Fim!')
+if pergunta == 3:
+    print ('\033[1;30;41m sua cor preferida', 'é' , 'Vermelho\033[m')
+    print ('Fim!')
+if pergunta == 4:
+    print ('\033[1;30;44m sua cor preferida', 'é' , 'Azul\033[m')
+    print ('Fim!')
+if pergunta == 5:
+    print ('\033[1;0;40m sua cor preferida', 'é' , 'Preto\033[m')
+    print ('Fim!')
+if pergunta == 6:
+
+    print ('\033[1;30;47m sua cor preferida', 'é' , 'Cinza\033[m')
+    print ('Fim!')
+if pergunta == 7:
+    print ('\033[1;30;93m sua cor preferida', 'é' , 'Coral\033[m')
+    print ('Fim!')
+if pergunta == 8:
+    print ('\033[1;30;107m sua cor preferida', 'é' , 'Branco\033[m')
+    print ('Fim!')
+if pergunta == 9:
+    print ('\033[1;30;45m sua cor preferida', 'é' , 'Lilás\033[m')
+    print ('Fim!')
+if pergunta == 10:
+    #195 é magenta claro=rosa
+    print ('\033[1;30;105m Sua cor preferida', 'é' , 'Rosa\033[m')
+    print ('Fim!')
+
